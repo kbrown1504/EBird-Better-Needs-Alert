12734f72bc359156e1d13e4c80970027b0f54856
kbrown1504 <keegqbrown@gmail.com>
2024-08-23 21:38:48 -0500

    working w/ the api

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DataService } from '../services/data.service';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,38 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  sightings: any[] = []
+
+  days = 1;
+  region = "Nebraksa"
+  onlyRare = false
+  onlyReviewed = false
+
+  constructor(private dataService: DataService) { }
+
+  ionViewDidEnter() {
+    // this.dataService.getAllObservations(41.25, -95.99).subscribe((res) => {
+    //   console.log(res)
+    //   this.sightings = res
+    // })
+  }
+
+  rareChange() {
+    if (!this.onlyRare) {
+      this.onlyReviewed = false
+    }
+  }
+
+  async search() {
+    if (this.onlyRare) {
+      var obs = this.dataService.getRareObservations(41.25, -95.99, this.days).subscribe((res) => {
+        this.sightings = res
+        obs.unsubscribe()
+      })
+    } else {
+      this.sightings = await this.dataService.getAllObservations(41.25, -95.99, this.days)
+      console.log(this.sightings)
+    }
+  }
 
 }
